12bc0e67d5ab4176288f76ce087aaf7c0b5c4aa3
ghziirmdhn <ghazii.rmdhn@gmail.com>
2024-10-07 00:59:50 +0700

    first commit

---
+++ data/data.js
@@ -0,0 +1,10 @@
+export const data = [
+    {
+      id: 1,
+      imageUrl: "https://media.licdn.com/dms/image/v2/D5603AQFIS0NuRTjQJw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1681777383264?e=1733961600&v=beta&t=SYLjTDdU5AgN68BCkIw2xx-2FmvYu4y8EU4KjvPgKkI",
+      nama: "Muhammad Ghazi Ramadhan",
+      nim: "21120122140114",
+    },
+  ];
+  
+  
